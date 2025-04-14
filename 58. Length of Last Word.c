fdac64870b5c9cc4dcd86f73dc35e504a80df7f7
Heitor Hayashi Ferrairo <heitorhayashiferrairo@gmail.com>
2025-04-14 16:54:05 -0300

    added coments to 58. Length of Last Word.c

--- 58. Length of Last Word.c
+++ 58. Length of Last Word.c
@@ -1,15 +1,15 @@
 int lengthOfLastWord(char *s) {
     int index = 0, count = 0;
 
-    while (s[index] != '\0')
+    while (s[index] != '\0') //get to the end of the string
         index++;
-    while (!((s[index] >= 'a' && s[index] <= 'z') || (s[index] >= 'A' && s[index] <= 'Z')))
+    while (!((s[index] >= 'a' && s[index] <= 'z') || (s[index] >= 'A' && s[index] <= 'Z'))) //goes back until find letters
         index--;
-    while (((s[index] >= 'a' && s[index] <= 'z') || (s[index] >= 'A' && s[index] <= 'Z')) && index > 0) {
+    while (((s[index] >= 'a' && s[index] <= 'z') || (s[index] >= 'A' && s[index] <= 'Z')) && index > 0) { //increases count for each letter while also going back
         index--;
         count++;
     }
-    if ((s[index] >= 'a' && s[index] <= 'z') || (s[index] >= 'A' && s[index] <= 'Z'))
+    if ((s[index] >= 'a' && s[index] <= 'z') || (s[index] >= 'A' && s[index] <= 'Z')) //check last position
         count++;
     return count;
 }
